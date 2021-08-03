2bfaff0a4c72205ec40b76c9c903de7b48574bb0
Gary B <gbagnall8@gmail.com>
2021-08-03 17:13:55 +1000

    Removed client only

--- client-only-react/src/components/CheckUser.tsx
+++
@@ -1,8 +0,0 @@
-import React, { useState, useEffect } from 'react';
-
-export const CheckUser = () => {
-  const [loading, setLoading] = useState(true);
-  const [user, setUser] = useState(null);
-
-  useEffect(() => {}, []);
-};
