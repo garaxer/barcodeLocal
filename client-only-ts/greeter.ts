bf05f1a94a2f8c94933f7ade8a16c9301e3ed6df
Gary B <gbagnall8@gmail.com>
2021-08-04 01:53:20 +1000

    Typescript only started

---
+++ client-only-ts/greeter.ts
@@ -0,0 +1,15 @@
+//tsc greeter.ts
+
+//Just the form with validation and sending the data somewhere
+interface Person {
+  firstName: string;
+  lastName: string;
+}
+
+function greeter(person: Person) {
+  return "Hello, " + person.firstName + " " + person.lastName;
+}
+
+let user = { firstName: "Jane", lastName: "User" };
+
+document.body.textContent = greeter(user);
